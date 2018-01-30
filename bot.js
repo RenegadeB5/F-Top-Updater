d878234838cffff0c9052e69a952fcfe6c199659
RenegadeB5 <rocketmanB5@gmail.com>
2018-01-30 10:41:58 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,21 +2,21 @@
 //anything with "//" infront of it is treated as a comment, it doesn't affect the code of the bot
 const Discord = require('discord.js');
 const config = require("./config.json");
-const client = new Discord.Client();
+var client = new Discord.Client();
 const prefix = "!";
 const args = message.content.slice(prefix.length).trim().split(/ +/g);
 var channel;
 
 client.on('ready', () => {
     client.user.setGame("Exiled Hack! " + client.guilds.array().length + " Servers");
-    console.log('successfully Logged In As Wall Check Bot!');
-    NOTIFY_CHANNEL = client.channels.find("name", "schems"); // Channel to send notification
+    console.log('successfully Logged In As schem Bot!');
+    channel = client.channels.find("name", "schems"); // Channel to send notification
 
 client.on ('message', message => {
   if (message.content === prefix + "schem") {
         let schem = args.slice(1).join(" ");
           
-              NOTIFY_CHANNEL.sendMessage(`Here you go.`, {
+              channel.sendMessage(`Here you go.`, {
   files: [
     "./schems/schem + ".schematic""
   ]
