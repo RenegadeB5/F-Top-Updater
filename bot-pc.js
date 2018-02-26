7d9b0596385b07ec557e8437aa7df6121dc0f0da
RenegadeB5 <rocketmanB5@gmail.com>
2018-02-26 13:03:06 -0500

    Update bot-pc.js

--- bot-pc.js
+++ bot-pc.js
@@ -6,30 +6,23 @@ const prefix = "!";
 var NOTIFY_CHANNEL;
 
 client.on('ready', () => {
-    client.user.setPresence({ game: { name: 'Updater', type: 0 } });
+    client.user.setPresence({ game: { name: 'Updating....', type: 0 } });
     console.log('successfully Logged In As Update Bot!');
-}); 
-
-client.on ('message', message => {
-  const args = message.content.slice(prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();  
-  const testFolder = './schems/';
-  const fs = require('fs');
-  if (command === "update") {
-      function remove() {
-               message.channel.send("test") }
-           setTimeout(remove, 3000)
+    NOTIFY_CHANNEL = client.channels.find("name", "ftop-update");  
+    const testFolder = './schems/';
+    const fs = require('fs');
       fs.readdirSync(testFolder).forEach(file => {
-           let file2 = "/app/schems/" + file
-           console.log(file2);
-           message.channel.send(`Here you go.`, {
-              files: [
-                 file2
-              ]
-            })
+         let file2 = "/app/schems/" + file
+         console.log(file2);
+         NOTIFY_CHANNEL.sendMessage(`Here you go.`, {
+            files: [
+               file2
+            ]
+          })
    
-      })
-}});
+    })
+}); 
 
 //LOGIN TOKEN-------------------------------------------------------------------
 client.login('NDA3NTkzODIzOTIxNzY2NDEw.DVIqzw.tVwiSN30_4x6LexUutYETZlyNAU');
+
