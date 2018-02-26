4dc9bb79001d6a0f8321cb4b7d042fe8a6ab6c0b
RenegadeB5 <rocketmanB5@gmail.com>
2018-02-26 10:14:58 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,8 +7,37 @@ var NOTIFY_CHANNEL;
 
 client.on('ready', () => {
     client.user.setPresence({ game: { name: process.env.IM_PLAYING, type: 0 } });
-    console.log('successfully Logged In As schem Bot!');
-});   
+    console.log('successfully Logged In As Update Bot!');
+}); 
+
+function getNewestFile(dir, regexp) {
+    var fs = require("fs"),
+     path = require('path'),
+    newest = null,
+    files = fs.readdirSync(dir),
+    one_matched = 0,
+    i
+
+    for (i = 0; i < files.length; i++) {
+
+        if (regexp.test(files[i]) == false)
+            continue
+        else if (one_matched == 0) {
+            newest = files[i];
+            one_matched = 1;
+            continue
+        }
+
+        f1_time = fs.statSync(path.join(dir, files[i])).mtime.getTime()
+        f2_time = fs.statSync(path.join(dir, newest)).mtime.getTime()
+        if (f1_time > f2_time)
+            newest[i] = files[i]  
+    }
+
+    if (newest != null)
+        return (path.join(dir, newest))
+    return null
+}
 
 client.on ('message', message => {
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
@@ -20,7 +49,7 @@ client.on ('message', message => {
                message.channel.send("test") }
            setTimeout(remove, 3000)
       fs.readdirSync(testFolder).forEach(file => {
-           let file2 = '/app/schems/' + file
+           let file2 = getNewestFile("/app/schems/", new RegExp('/app/schems/*\schematic'))
            console.log(file2);
            message.channel.send(`Here you go.`, {
               files: [
