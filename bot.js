3348a8d95762d6a98cdd20759340b99223ad0c17
RenegadeB5 <rocketmanB5@gmail.com>
2018-02-26 12:52:01 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,28 +8,26 @@ var NOTIFY_CHANNEL;
 client.on('ready', () => {
     client.user.setPresence({ game: { name: process.env.IM_PLAYING, type: 0 } });
     console.log('successfully Logged In As Update Bot!');
-}); 
-
-client.on ('message', message => {
-  const args = message.content.slice(prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();  
-  const testFolder = './schems/';
-  const fs = require('fs');
-  if (command === "update") {
-      function remove() {
-               message.channel.send("test") }
-           setTimeout(remove, 3000)
+    NOTIFY_CHANNEL = client.channels.find("name", "ftop-update");  
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();  
+    const testFolder = './schems/';
+    const fs = require('fs');
+  
+    function remove() {
+             message.channel.send("test") }
+         setTimeout(remove, 3000)
       fs.readdirSync(testFolder).forEach(file => {
-           let file2 = "/app/schems/" + file
-           console.log(file2);
-           message.channel.send(`Here you go.`, {
-              files: [
-                 file2
-              ]
-            })
+         let file2 = "/app/schems/" + file
+         console.log(file2);
+         NOTIFY_CHANNEL.sendMessage(`Here you go.`, {
+            files: [
+               file2
+            ]
+          })
    
-      })
-}});
+    })
+}); 
 
 //LOGIN TOKEN-------------------------------------------------------------------
 client.login(process.env.BOT_TOKEN);
